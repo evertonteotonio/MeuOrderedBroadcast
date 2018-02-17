7d0618176786eb154b4759386eb3d4cc5b6ab8bd
logonrm <logonrm@fiap.com.br>
2018-02-17 11:23:41 -0200

    Final

--- app/src/main/java/com/example/logonrm/demoorderedbroadcast/MainActivity.kt
+++ app/src/main/java/com/example/logonrm/demoorderedbroadcast/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.logonrm.demoorderedbroadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var receiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
 
         /*
         btnEnviar.setOnClickListener({
@@ -20,5 +31,37 @@ class MainActivity : AppCompatActivity() {
         btnEnviar.setOnClickListener {
             sendBroadcast(Intent("com.example.logonrm.demoorderedbroadcast.MeuOutroRceiver"))
         }
+
+
+        ///////
+
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (intent?.action.equals(Intent.ACTION_POWER_CONNECTED)){
+                }else{
+                    tvUSBstatus.text = "Cabo conectado"
+                }
+                tvUSBstatus.text = "Cabo desconectado"
+            }
+        }
+
+        /*
+        when(intent?.action){
+            Intent.ACTION_POWER_CONNECTED -> tvUSBstatus.text = "Cabo conectado"
+            Intent.ACTION_POWER_DISCONNECTED -> tvUSBstatus.text = "Cabo desconectado"
+        }
+        */
+
+        registerReceiver(receiver, filter)
+
+
     }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
+    }
+
+
+
 }
